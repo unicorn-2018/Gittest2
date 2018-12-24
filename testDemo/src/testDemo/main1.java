45c8ad0840954d94b3b01d8226756219227f60df
inoue <inoue@test>
2018-12-24 12:22:52 +0900

    デモ用プログラム1

---
+++ testDemo/src/testDemo/main1.java
@@ -0,0 +1,19 @@
+package testDemo;
+
+/**
+ *  GIT テスト・デモ用リポジトリ
+ *
+ *
+ *
+ * **/
+
+public class main1 {
+
+	public static void main(String[] args) {
+		// TODO メッセージ出力 ユーザ毎に1行追加
+		// ※他ユーザのメッセージを上書きしないこと
+		System.out.printf("test1\n");
+		System.out.printf("s.inoue");
+	}
+
+}
