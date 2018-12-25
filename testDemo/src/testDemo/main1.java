af854eeff16880a33b20ac951df4716df3c4f9f3
Administrator <Administrator@WIN-EC2>
2018-12-25 20:44:02 +0900

    テスト２プル

--- testDemo/src/testDemo/main1.java
+++ testDemo/src/testDemo/main1.java
@@ -15,6 +15,7 @@ public class main1 {
 		System.out.printf("test1\n");
 		System.out.printf("s.inoue");
 		System.out.printf("テスト1：内容の更新（コミット、プッシュ）");
+		System.out.printf("テスト2：内容の更新（プル）");
 
 	}
 
